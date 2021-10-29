1eb94f75a82ab23e7b4d8353c3328ca3e17ff866
root <ChoukriAbdellah>
2021-10-29 18:05:23 +0200

    Updates

---
+++ precache-manifest.34ed76e5072c77b3c261e100d8ccf251.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201b922d2c34ff1c51805626324259a3",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "5878cdc7634244c5f07f",
+    "url": "/developerFolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "36b080b5b1dee5a0b6ed",
+    "url": "/developerFolio/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "4ae906a7fb25dc89501d",
+    "url": "/developerFolio/static/css/main.a021952d.chunk.css"
+  },
+  {
+    "revision": "d99f28d5a693c6a0d10e",
+    "url": "/developerFolio/static/js/2.42c53459.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/developerFolio/static/js/2.42c53459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5878cdc7634244c5f07f",
+    "url": "/developerFolio/static/js/3.c5375608.chunk.js"
+  },
+  {
+    "revision": "36b080b5b1dee5a0b6ed",
+    "url": "/developerFolio/static/js/4.afe8fb20.chunk.js"
+  },
+  {
+    "revision": "4ae906a7fb25dc89501d",
+    "url": "/developerFolio/static/js/main.c1ec992b.chunk.js"
+  },
+  {
+    "revision": "ccb7c525f24d7515197a",
+    "url": "/developerFolio/static/js/runtime-main.72a8da75.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "5e590e93091967e915a537eb844fb3c2",
+    "url": "/developerFolio/static/media/LJG.5e590e93.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "9a23815918b56a30290b9cce5b3a2344",
+    "url": "/developerFolio/static/media/blog-logo.9a238159.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/developerFolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/developerFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8d27d9db78f3fe7631547dd44660d313",
+    "url": "/developerFolio/static/media/diko-logo.8d27d9db.png"
+  },
+  {
+    "revision": "91dfd8b8f53c3067fce563f4a556664e",
+    "url": "/developerFolio/static/media/football-api.91dfd8b8.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/developerFolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "7f00cc128ddaf06aac837fde7543b886",
+    "url": "/developerFolio/static/media/logo-fds.7f00cc12.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "0abd3bf764f7a6910313ec6c8c4e77d9",
+    "url": "/developerFolio/static/media/portfolio-logo.0abd3bf7.jpg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/developerFolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "42e1b27a63cf0eb720d91eb483717ecd",
+    "url": "/developerFolio/static/media/school-plus-logo.42e1b27a.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/developerFolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
